6a609b9fbc6221d86d2aabf6af63f3712822c3e0
Mark Perov <mark.perov@stud.fh-campuswien.ac.at>
2022-01-10 20:09:44 +0100

    Help Button funktional und wieder zurück

---
+++ src/main/java/at/ac/fhcampuswien/coffescript/mmmmmcrawler/HelpController.java
@@ -0,0 +1,27 @@
+package at.ac.fhcampuswien.coffescript.mmmmmcrawler;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class HelpController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void goToLandingPage(ActionEvent event) throws IOException {
+        //navigation zum zurück zur Landing Page
+        Parent root = FXMLLoader.load(getClass().getResource("landing-page.fxml"));
+        stage =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
