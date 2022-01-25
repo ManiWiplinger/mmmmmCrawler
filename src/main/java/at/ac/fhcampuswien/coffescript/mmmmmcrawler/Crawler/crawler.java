1bfcff446b87afbbeef61f071e1675557da89628
Mathias Palfinger <mathias.palfinger@stud.fh-campuswien.ac.at>
2022-01-25 23:03:04 +0100

    Letzter Glow entfernt

--- src/main/java/at/ac/fhcampuswien/coffescript/mmmmmcrawler/Crawler/crawler.java
+++ src/main/java/at/ac/fhcampuswien/coffescript/mmmmmcrawler/Crawler/crawler.java
@@ -1,5 +1,7 @@
 package at.ac.fhcampuswien.coffescript.mmmmmcrawler.Crawler;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
